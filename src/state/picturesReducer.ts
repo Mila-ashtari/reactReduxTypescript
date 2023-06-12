69bbf3dc2a05c0449420605dd3878fc734498a22
Mila-ashtari <mashtari1991@gmail.com>
2023-06-12 11:40:36 -0400

    annotating reducer

--- src/state/picturesReducer.ts
+++ src/state/picturesReducer.ts
@@ -1,8 +1,23 @@
-interface picturesState{
-    loading: boolean;
-    error: boolean | null;
-    data:string[]
+interface picturesState {
+  loading: boolean;
+  error: boolean | null;
+  data: string[];
 }
-const picturesReducer= (state: picturesState, action: any)=>{
 
+interface action {
+  type: string;
+  payload?: any;
 }
+const picturesReducer = (state: picturesState, action: any): picturesState => {
+  switch (action.type) {
+    case "FETCH_PICTURES_SUCCESS":
+      return { loading: false, error: null, data: action.payload };
+    case "FETCH_PICTURES_LOADING":
+      return { loading: true, error: null, data: [] };
+    case "FETCH_PICTUES_ERROR":
+      return { loading: false, error: null, data: [] };
+
+    default:
+      return state;
+  }
+};
