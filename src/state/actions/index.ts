6d3135cee4e628f0e9b23e74359a49e4af3ec21f
Mila-ashtari <mashtari1991@gmail.com>
2023-06-13 15:15:34 -0400

    creating actions

---
+++ src/state/actions/index.ts
@@ -0,0 +1,6 @@
+import axios from 'axios'
+import { PicturesActionType } from '../actionTypes'
+
+const fetchPictures=()=>{
+    
+}
--- src/state/reducers/picturesReducer.ts
+++ src/state/reducers/picturesReducer.ts
@@ -1,3 +1,5 @@
+import { PicturesActionType } from "../actionTypes";
+
 interface picturesState {
   loading: boolean;
   error: boolean | null;
@@ -10,11 +12,11 @@ interface action {
 }
 const picturesReducer = (state: picturesState, action: action): picturesState => {
   switch (action.type) {
-    case "FETCH_PICTURES_SUCCESS":
+    case PicturesActionType.FETCH_PICTURES_SUCCESS:
       return { loading: false, error: null, data: action.payload };
-    case "FETCH_PICTURES_LOADING":
+    case PicturesActionType.FETCH_PICTURES_LOADING:
       return { loading: true, error: null, data: [] };
-    case "FETCH_PICTUES_ERROR":
+    case PicturesActionType.FETCH_PICTUES_ERROR:
       return { loading: false, error: null, data: [] };
 
     default:
